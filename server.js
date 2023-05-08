2fbe32e7598a30a5a1101507389ddf3d5b3b89ad
Mateusz Kretowski <matkretkurs@gmail.com>
2023-05-08 20:53:08 +0200

    Fix bug

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.static(path.join(__dirname, '/client/build'))); // Serve static 
 app.use(express.urlencoded({ extended: false })); //x-www-form-urlencoded
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/client/index.html'));
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
 });
 
 app.use((req, res) => {
